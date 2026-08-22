6201da9c0094ed41629a3da2c52d82a1fc558067
Mohammed <Mohammed@MOHAMMED-PC>
2026-08-22 09:53:26 +0300

    Constructorless Practicng

--- Program.cs
+++ Program.cs
@@ -1,92 +1,31 @@
 ﻿using System;
 
-namespace Calculator
+class clsPersos
 {
-   class clsCalculator
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public clsPersos()
     {
-        private float _counter = 0;
-        private float _LastNumber = 0;
-        private string _LastOperation = "Clear";
-
-        private bool _ValidateNOTZero (float Number)
-        {
-            return (Number <= 0);
-        }
-        public void Add (float number )
-        {
-            _LastNumber = number;
-            _LastOperation = "Adding";
-            _counter += number;
-        }
-
-        public void Subtract (float number )
-        {
-            _LastNumber -= number;
-            _LastOperation = "Subtracting";
-_counter -= number;
-        }
-
-        public void Multiply (float number)
-        {
-            _LastNumber = number;
-            _LastOperation = "Multiplying";
-            _counter *= number;
-        }
-        public void Divide(float number)
-        {
-
-            bool Succeeded = true;
-            _LastOperation = "Dividing";
-
-            if (_ValidateNOTZero(number))
-            {
-                _LastNumber = number;
-                _counter /= 1;
-                Succeeded = false;
-
-            }
-            else
-            {
-                _LastNumber = number;
-                _counter /= number;
-            }
-
-
-            _counter = _counter / number;
-        }
-
-        public void Clear ()
-        {
-            _counter = 0;
-        }
-
-        public float GetFinalResult()
-        {
-            return _counter;
-        }
-
-        public void PrintReslt()
-        {
-            Console.WriteLine("{0}", _counter);
-        }
-
+        ID = -1;
+        Name = "Empty";
+        Age = 0;
     }
+}
 
-    internal class Program
+internal class Program
+{
+    static void Main (string[] args)
     {
 
-        public static void Main(string[] args)
-        {
-            clsCalculator calculator1  =  new clsCalculator();
+        clsPersos Person1 = new clsPersos();
 
-            calculator1.Add(10);
-            calculator1.Multiply(10);
-            Console.WriteLine(calculator1.GetFinalResult());
+        Console.WriteLine("ID   = {0}", Person1.ID);
+        Console.WriteLine("Name = {0}", Person1.Name);
+        Console.WriteLine("Age  = {0}", Person1.Age);
 
-        }
-    
-    
+        Console.ReadKey();
     }
-
 
 }
