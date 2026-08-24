d35209f3ebff4978c0ac59e09529e360dd5ef8e7
Mohammed <Mohammed@MOHAMMED-PC>
2026-08-24 11:03:28 +0300

    Read Use of Static Example

--- Program.cs
+++ Program.cs
@@ -1,47 +1,85 @@
-﻿using System;
-
-class Settings
+﻿
+using System;
+namespace Practice
 {
-    public static int DayNumber
-    {
-        get
+    class clsPerson
+    { 
+       public int ID { get; set; }
+       public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public clsPerson (int iD, string name, int age)
         {
-            return DateTime.Today.Day;
+            ID = iD;
+            Name = name;
+            Age = age;
+           
+        }
+
+
+
+
+        public static clsPerson Find (int Id)
+        {
+            if (Id== 10)
+            {
+                return new clsPerson(10, "Mohammed", 18);
+            }
+            else
+            {
+                return null;
+            }
+
+
         }
     }
-
-    public static string DayName
-    {
-        get
-        {
-            return DateTime.Today.DayOfWeek.ToString();
-        }
-    }
-    public static string ProjectPath
-    {
-        get;
-        set;
-    }
-
-    static Settings ()
-    {
-        ProjectPath = string.Empty; 
-    }
-
-
-}
 
 internal class Program
-{
-    static void Main()
     {
-        //if We Use Static class or Private Constructor We Can Create an object of this class ;
-        // Settings Obj1 = new Settings();
+        static void Main (string[] args)
+        {
+            clsPerson Person1 = new clsPerson(612, "Mohammed Ibrahim", 18);
 
-        Console.WriteLine(Settings.DayNumber);
-        Console.WriteLine(Settings.DayName);
+            Console.WriteLine("Finding Person 1 By ID");
+
+            clsPerson Person2 = clsPerson.Find(10); 
+
+
+
+            if (Person2 != null)
+            {
+                Console.WriteLine("ID   =  {0}", Person2.ID);
+                Console.WriteLine("Name =  {0}" , Person2.Name);
+                Console.WriteLine("Age  =  {0}", Person2.Age);
+
+                Console.ReadKey();
+
+            }
+
+            else
+            {
+                Console.WriteLine("Could Not Find The Person By Giving ID");
+            }
+
+
+
+
+
+        }
+
+
+
+
 
     }
+
+
+
 
 
 }
