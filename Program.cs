ce15335409d28dafcaf934ffc132624558d7b502
Mohammed <Mohammed@MOHAMMED-PC>
2026-08-24 13:36:06 +0300

    Inherite Constructor

--- Program.cs
+++ Program.cs
@@ -1,37 +1,54 @@
 ﻿
 using System;
+using System.ComponentModel;
 
 namespace Practice
 {
 
     // Base Class / Super Class
 
-public class clsPerson
+    public class clsPerson
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
-    public string LastName { get; set; }
+        public string LastName { get; set; }
         public string Title { get; set; }
 
         public string FullName
         {
-        get
+            get
             {
-                return FirstName  +  " " + LastName;
+                return FirstName + " " + LastName;
             }
+        }
+
+        public clsPerson(int id  ,  string Firstname ,  string lastname  ,string title)
+        {
+            this.ID = id;
+            this.FirstName =  Firstname;
+            this.LastName = lastname;
+            this.Title  = title;
+
         }
 
     }
 
     //Sub Class  / Dervied Class
     public class clsEmployee :  clsPerson 
-    {
+    { 
     public float Salary { get; set; }
     public string DepartmentName { get; set; }
     
     public void IncreaseSalaryBy (float Amount)
         {
             Salary += Amount;
+        }
+
+        public clsEmployee (int id, string Firstname, string lastname, string title , float Salary ,string Departmentname )
+            : base (id  ,  Firstname  , lastname , title)
+        {
+            this.Salary = Salary;
+            this.DepartmentName = Departmentname;
         }
    
     }
@@ -41,16 +58,7 @@ public class clsPerson
     {
         static void Main ()
         {
-            clsEmployee Employee1 = new clsEmployee ();
-
-            Employee1.FirstName = "Mohammed";
-            Employee1.LastName = "Ibrahim";
-            Employee1.Salary = 5000;
-            Employee1.ID = 612;
-            Employee1.DepartmentName = "IT";
-            Employee1.Title = "Mr";
-
-
+            clsEmployee Employee1 = new clsEmployee (512,"Mohammed" , "Ibrahim"   ,"Mr" ,5000 , "IT");
 
             Console.WriteLine("ID             = {0}", Employee1.ID);
             Console.WriteLine("Full Name      = {0}", Employee1.FullName);
