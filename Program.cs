b95387cb62aa3b30cf25ebff29a254d108d5c521
Mohammed <Mohammed@MOHAMMED-PC>
2026-08-29 14:32:35 +0300

    Interfaces Practice

--- Program.cs
+++ Program.cs
@@ -1,82 +1,50 @@
-
 using System;
-using System.Runtime.InteropServices;
-using System.Runtime.Intrinsics.X86;
 
-namespace Main
+namespace Practice
 {
-public class clsA
+ public interface IConstract
     {
-        public string Name { get; set; }
-        public int Id { get; set; }
+      string FirstName { set; get; }
+        string LastName { set; get; }
 
-      public virtual void Cheers ()
-        {
-            Console.WriteLine($"Hello It is {Name} My ID is {Id}");
-        }
+        void Printer(string Name , int age );
 
-        public virtual void Question ()
-        {
-            Console.WriteLine("What is your Name ?");
-        }
-
-
+        void Greet();
     }
 
-    public class clsB : clsA
+
+    public class clsEmployee : IConstract
     {
-        public string phone { get; set; } 
-        public string Email { get; set; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
 
-        public override void Cheers()
+        public void Printer(string Name, int age)
         {
-            Console.WriteLine($"My PhoneNumber is {phone} and my Email is {Email}");
+            Console.WriteLine($"Hello Iam {Name} and Iam {age} Years Old");
         }
 
-        public new void Question()
+        public void Greet()
         {
-            base.Question();
-            Console.WriteLine("What is your Email ?");
-            Console.WriteLine("What is your Phone Number ?");
+            Console.WriteLine("Hello Every Body");
         }
+
     }
 
 
-   internal class Program
-    { 
-    static void Main (string[] args)
+
+    internal class Program
+    {
+    static public void Main (string[] args)
         {
-          clsA A1 = new clsA { Name = "Mohammed" , Id = 312};
-            A1.Cheers();
-            A1.Question();
+          clsEmployee employee = new clsEmployee {FirstName = "Mohammed" , LastName = "Ibrahim" };
+            employee.Greet();
+
+            employee.Printer("Mohammed Ibrahim", 18);
             Console.ReadKey();
-            Console.WriteLine("\n");
-            clsB B1 = new clsB();
-            B1.Cheers();
-            B1.Question();
-
-            Console.ReadKey();
-            Console.WriteLine("\n\n");
-
-           clsB B2 = new clsB();
-            clsA A2 = B2;
-
-            A2.Cheers();
-            A2.Question();
         }
     
     
-    
-    
-    
-    
-    
-    
-    
     }
-
-     
-
 
 
 
